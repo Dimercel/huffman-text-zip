544eb3cd503f918644ca55e5e5fdb0a99c03eb68
Dimercel <xolcman@gmail.com>
2026-03-02 17:11:09 +0300

    Basic data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
+
+use easy_tree::Tree;
+
+type FreqMap = HashMap<char, u32>;
+
+#[derive(Debug)]
+enum Bit {One, Zero}
+
+type Code = Vec<Bit>;
+type CodeMap = HashMap<char, Code>;
+type CharWeight = (u32, char);
+
+fn build_tree(freq: &FreqMap) -> Tree<CharWeight> {
+    Tree::new()
+}
+
+
 fn main() {
     println!("Hello, world!");
+    println!("{:?}", Bit::One);
+
 }
